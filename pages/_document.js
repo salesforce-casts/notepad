d10166f783ae40567c5ed7caf04b557bb5c581cb
salesfore-casts <teja@salesforcecasts.com>
2023-08-03 01:36:41 +0530

    added analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+			<script
+				defer
+				data-domain="iseditor.com"
+				src="http://data.salesforcecasts.com/js/script.js"
+			></script>
+		</Html>
+	);
 }

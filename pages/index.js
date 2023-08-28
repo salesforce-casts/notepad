96b193ec5311299bb0a967544ab24081a1a5d9d0
sohanemon <m.sohanemon@gmail.com>
2023-08-28 23:14:42 +0600

    feat: text storing to LS

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,50 @@
-import React, { useRef } from 'react';
-import Editor, { Monaco } from '@monaco-editor/react';
+import useTextStore from '@/lib/text-store';
+import Editor from '@monaco-editor/react';
 import Head from 'next/head';
+import { useRef } from 'react';
 
 const App = () => {
-	const editorRef = useRef(null);
+  const editorRef = useRef(null);
 
-	function handleEditorDidMount(editor, monaco) {
-		// here is the editor instance
-		// you can store it in `useRef` for further usage
+  function handleEditorDidMount(editor, monaco) {
+    // here is the editor instance
+    // you can store it in `useRef` for further usage
 
-		editorRef.current = editor;
-	}
+    editorRef.current = editor;
+  }
 
-	return (
-		<div className=" relative">
-			{/* Add your meta tags here */}
-			<Head>
-				<title>Free online notepad app</title>
-				<meta
-					name="description"
-					content="Free online notepad app. No login required, 100% Free."
-				/>
-				{/* Other meta tags */}
-			</Head>
-			{/* <h2 className="block fixed text-2xl text-right text-white z-50 font-mono">
+  //   global hooks
+  const [text, setText] = useTextStore((s) => [s.text, s.setText]);
+
+  return (
+    <div className=' relative'>
+      {/* Add your meta tags here */}
+      <Head>
+        <title>Free online notepad app</title>
+        <meta
+          name='description'
+          content='Free online notepad app. No login required, 100% Free.'
+        />
+        {/* Other meta tags */}
+      </Head>
+      {/* <h2 className="block fixed text-2xl text-right text-white z-50 font-mono">
 				built by teja
 			</h2> */}
-			<Editor
-				height="100vh"
-				defaultLanguage="Markdown"
-				theme="vs-dark"
-				defaultValue="// some comment"
-				onMount={handleEditorDidMount}
-				options={{
-					scrollBeyondLastLine: false,
-					fontSize: '30px',
-				}}
-				className="absolute top-0 left-0 right-0 bottom-0 rounded"
-			/>
-		</div>
-	);
+      <Editor
+        height='100vh'
+        onChange={setText}
+        defaultLanguage='Markdown'
+        theme='vs-dark'
+        value={text}
+        onMount={handleEditorDidMount}
+        options={{
+          scrollBeyondLastLine: false,
+          fontSize: '30px',
+        }}
+        className='absolute top-0 left-0 right-0 bottom-0 rounded'
+      />
+    </div>
+  );
 };
 
 export default App;

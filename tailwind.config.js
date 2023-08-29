218a17a095c9318be00d0a6ebf2c39bd02085236
sohanemon <m.sohanemon@gmail.com>
2023-08-29 13:44:33 +0600

    feat: animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,40 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        text: '#d4d4d4',
+        bg: '#1e1e1e',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        dash: 'dash 1.5s ease-in-out infinite',
+        rotate: 'rotate 2s linear infinite',
+      },
+      keyframes: {
+        dash: {
+          '0%': {
+            strokeDasharray: '1, 200',
+            strokeDashoffset: '0',
+          },
+          '50%': {
+            strokeDasharray: '90, 200',
+            strokeDashoffset: '-35px',
+          },
+          '100%': {
+            strokeDashoffset: '-125px',
+          },
+        },
+        rotate: {
+          '100%': {
+            transform: 'rotate(360deg)',
+          },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
